e784a3dcbbee563907251aefeff4b28fa58d0f44
Kevin Sayago <120409494+Kevin-Sayago312@users.noreply.github.com>
2025-02-08 16:54:18 -0600

    Primer commit para el proyecto de BarberiaAppWeb

---
+++ database/factories/CitaFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Cita>
+ */
+class CitaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'fecha' => $this -> faker ->date('Y-m-d'),
+            'hora' => $this -> faker -> time('H:i:s'),
+            'userid' => User::factory(),
+        ];
+    }
+}
